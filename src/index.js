c0d18964d518b71ab9d3fb50584d49fa7d8384b2
Bogdan Kazaryan <bogdankazaryan@yandex.ru>
2018-02-25 21:04:31 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,86 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.compare = (left, right) => left - right;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    let sortedArray = [];
+    indices.sort(
+      (left, right) => left - right
+    );
+    for (let i = 0; i < indices.length; i++){
+      sortedArray.push(this.array[indices[i]]);
+    }
+    sortedArray.sort(this.compare);
+    for (let j = 0; j < indices.length; j++){
+      this.array[indices[j]] = sortedArray[j];
+    } 
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compare = compareFunction;
   }
 }
 
 module.exports = Sorter;
+
+// class Sorter {
+//   constructor(addedElement) {
+//     this.array = [];
+//     this.sortF = function compareFunction(a, b){
+//       return a - b;
+//     };
+//   }
+
+//   add(element) {
+//     this.array.push(element);
+//   }
+
+//   at(index) {
+//     return this.array[index];
+//   }
+
+//   get length() {
+//     return this.array.length;
+//   }
+
+//   toArray() {
+//     return this.array;
+//   }
+
+//   sort(indices) {
+//     var sortedArray = [];
+//     indices.sort( (a,b) => a - b );
+
+//     for(var i = 0; i < indices.length; i++){
+//       sortedArray.push(this.array[indices[i]]);
+//     }
+//     sortedArray.sort(this.sortF);
+//     for(var j = 0; j < indices.length; j++){
+//       this.array[indices[j]] = sortedArray[j];
+//     }
+//   }
+
+//   setComparator(compareFunction) {
+//     this.sortF = compareFunction;
+//   }
+// }
+
+// module.exports = Sorter;
